9625ac4285ebddb12616e1f5eff819997d908d9f
Jaac25 <acevedoalexander52@gmail.com>
2022-05-26 13:01:44 -0500

    Correcciones de warnings, corrección del paso inicial y info readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Correr:
 1. npm i para instalar dependencias.
 2. npm start para correr el proyecto
+
+Si se intenta saltar un paso, este validará los pasos intermedios y anteriores y lo redigirá al paso inicial.
--- src/pages/start/start.tsx
+++ src/pages/start/start.tsx
@@ -1,16 +1,11 @@
 import { Button } from "../../components/Button/Button";
 import { Container } from "../../components/container/Container";
-import { Input } from "../../components/Input/Input";
-import { Resume } from "../../components/Resume/Resume";
-import { store } from "../../layers/store";
 import "./style.css"
 
-export const Start = ({}) => {
-    const {nextStep} = store(state => state)
-    
+export const Start = () => {
     return (
         <Container size={100}>
-            <Button text="BIENVENIDO" disabled={false} to="/dataClient" onClick={() => nextStep()}></Button>
+            <Button text="BIENVENIDO" disabled={false} to="/dataClient" onClick={() => {}}></Button>
         </Container>
     )
 }
